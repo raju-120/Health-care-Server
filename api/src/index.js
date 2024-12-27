1e6487c027f9cf11747789f47b9cb2e545ff2ad0
raju-120 <ashfaqraju27@gmail.com>
2024-12-27 14:31:28 +0600

    cors changed

--- api/src/index.js
+++ api/src/index.js
@@ -21,13 +21,13 @@ import ComplainedBox from "./routes/coomplain.route.js";
 import Prescriptions from "./routes/prescription.route.js";
 import PatientRegForm from "./routes/patient.route.js";
 
-dotenv.config(); // Default `.env` file at the root
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || "*" })); // Use deployed frontend URL in `CLIENT_URL`
+app.use(cors({ origin: process.env.CLIENT_URL || CORS_ORIGIN })); 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
