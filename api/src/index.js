2c7f2bf255281c0fa328b142cff37e5869bdb1b1
raju-120 <ashfaqraju27@gmail.com>
2024-12-27 14:42:51 +0600

    cors origin  changed

--- api/src/index.js
+++ api/src/index.js
@@ -27,7 +27,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || CORS_ORIGIN })); 
+app.use(cors({ origin: process.env.CLIENT_URL || process.env.CORS_ORIGIN })); 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
